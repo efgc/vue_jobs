1d2463940c142db8b2e6b4322caf000e7c9ac9c4
ErickG <efgc.dev@gmail.com>
2025-09-08 21:03:32 -0600

    first commit, almost finished project

---
+++ src/components/JobListings.vue
@@ -0,0 +1,46 @@
+<script setup>
+import JobListing from '@/components/JobListing.vue';
+//import jobData from '@/jobs.json';
+import { reactive, ref, defineProps, onMounted } from 'vue';
+import axios from 'axios';
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean, default: false
+  }
+});
+
+const state = reactive({
+  jobs: [],
+  isLoading: true
+
+});
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('http://localhost:5000/jobs');
+    state.jobs = response.data;
+    //jobs.value = response.data;
+  } catch (error) {
+    console.log('Error fetching jobs', error);
+  } finally {
+    state.isLoading = false;
+  }
+});
+</script>
+
+<template>
+  <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
+      <div class="grid grid-cols-1 gap-6 md:grid-cols-3">
+        <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
+      </div>
+    </div>
+  </section>
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <RouterLink to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All
+      Jobs</RouterLink>
+  </section>
+</template>
